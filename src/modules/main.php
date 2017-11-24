bc6f83430a403b3716f12c98a2a760d3ba1fda77
Roman Lukoyanov <ramzes.2007.90@gmail.com>
2017-11-24 16:48:40 +0300

    Starts

--- src/modules/main.php
+++ src/modules/main.php
@@ -1,8 +1,8 @@
 <div class="main">
     <div class="carousel carousel-slider">
-        <a class="carousel-item" href="#one!"><img src="images/1.jpg"></a>
-        <a class="carousel-item" href="#two!"><img src="images/2.jpg"></a>
-        <a class="carousel-item" href="#three!"><img src="images/3.jpg"></a>
-        <a class="carousel-item" href="#four!"><img src="images/4.jpg"></a>
+        <a class="carousel-item" href="#one!"><img src="images/slider/1.jpg"></a>
+        <a class="carousel-item" href="#two!"><img src="images/slider/2.jpg"></a>
+        <a class="carousel-item" href="#three!"><img src="images/slider/3.jpg"></a>
+        <a class="carousel-item" href="#four!"><img src="images/slider/4.jpg"></a>
     </div>
 </div>
